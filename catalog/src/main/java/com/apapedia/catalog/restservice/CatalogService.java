5da573cccda87be6d85a0e3a51819869b73a3112
Adam Wilson Autan <adam.wilson@ui.ac.id>
2023-12-04 13:41:57 +0700

    fix: duplicate interface method definitions

--- catalog/src/main/java/com/apapedia/catalog/restservice/CatalogService.java
+++ catalog/src/main/java/com/apapedia/catalog/restservice/CatalogService.java
@@ -8,50 +8,20 @@ public interface CatalogService {
 
     void createRestCatalog(Catalog catalog);
     List<Catalog>retrieveAllCatalog();
-    
     Catalog getCatalogById(UUID id);
     Catalog updateCatalog (Catalog updatedCatalog);
-
     List<Catalog>getAllCatalogsBySellerId(UUID sellerId);
-
     List<Catalog> getCatalogListByCatalogName(String productName);
-
     List<Catalog> getCatalogByPrice(int minPrice, int maxPrice);
-
     List<Catalog> getSortedCatalogList(String sortBy, String order);
-
     List<Catalog> sortByProductNameAsc();
-
     List<Catalog> sortByProductNameDesc();
-
     List<Catalog> sortByPriceAsc();
-
     List<Catalog> sortByPriceDesc();
-
-
-
-
-
-    List<Catalog> getCatalogListByCatalogName(String productName);
-
-    List<Catalog> getCatalogByPrice(int minPrice, int maxPrice);
-
     void hardDeleteCatalog(UUID id);
-
     void deleteCatalog(UUID id);
 
-    void deleteCatalog(UUID id);
 
-    void hardDeleteCatalog(UUID id);
 
-    List<Catalog> getSortedCatalogList(String sortBy, String order);
-
-    List<Catalog> sortByProductNameAsc();
-
-    List<Catalog> sortByProductNameDesc();
-
-    List<Catalog> sortByPriceAsc();
-
-    List<Catalog> sortByPriceDesc();
 
 }
